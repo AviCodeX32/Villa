2e9b803c1b50dffa857bd68a1a09d07fe50b56ba
AviCodeX32 <avnishmaurya3223@gmail.com>
2026-08-26 13:27:28 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5175',
   'http://localhost:5174',
-  'http://127.0.0.1:4000',
+  'https://villa-aveee1.vercel.app',
 ];
 
 app.use(
